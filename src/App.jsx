5b466df28c07103b23416be6e482fe4deec91953
sadiarahmansmrity0 <sadiarahmansmrity9@gmail.com>
2026-03-30 23:15:40 +0600

    first commit with tailwind & daisy setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./component/Navbar"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <h1 className="text-4xl underline">Hello!</h1>
+    </>
+  )
+}
+
+export default App
