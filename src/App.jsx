6ab06dc5d123738abedb8e777826885b64d7307a
sadiarahmansmrity0 <sadiarahmansmrity9@gmail.com>
2026-04-01 20:06:02 +0600

    did the stat (steps) part

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar"
 import Banner from  "./components/Banner";
+import Stats from "./components/Stats";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
       <Navbar cartCount={0} /> 
       <Banner />
+      <Stats />
     </div>
     </>
   )
